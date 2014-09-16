aa4b2a240a8c98dfa19bcaaeba5dca5f1969146f
Mary Bucknell <mbucknell@usgs.gov>
2014-09-16 14:03:33 -0500

    Try this again ... so if spacing is fixed.

--- gcmrc-ui/src/main/webapp/app/reachdata.js
+++ gcmrc-ui/src/main/webapp/app/reachdata.js
@@ -1,102 +1,102 @@
 GCMRC.Reaches = function () {
 
-    var slider_defaults = {
-	bedLoad: {
-	    name: "a",
-	    displayName: "Bedload Coefficient for River Sand Loads",
-	    adjustMin: 0,
-	    adjustMax: 10,
-	    adjustDefault: 5
-	},
-	riverFinesLoad: {
-	    name: "e",
-	    displayName: "Magnitude of Possible Persistent Bias in Measured River Silt and Clay Loads",
-	    adjustMin: 0,
-	    adjustMax: 25,
-	    adjustDefault: 5
-	},
-	riverLoad: {
-	    name: "b",
-	    displayName: "Magnitude of Possible Persistent Bias in Measured River Sand Loads",
-	    adjustMin: 0,
-	    adjustMax: 25,
-	    adjustDefault: 5
-	},
-	majorTribFinesLoad: {
-	    name: "f",
-	    displayName: "Magnitude of Possible Persistent Bias in Measured Major Tributary Silt and Clay Loads",
-	    adjustMin: 0,
-	    adjustMax: 25,
-	    adjustDefault: 10
-	},
-	majorTribLoad: {
-	    name: "c",
-	    displayName: "Magnitude of Possible Persistent Bias in Measured Major Tributary Sand Loads",
-	    adjustMin: 0,
-	    adjustMax: 25,
-	    adjustDefault: 10
-	},
-	minorTribFinesLoad: {
-	    name: "g",
-	    displayName: "Magnitude of Possible Persistent Bias in Lesser Tributary Silt and Clay Loads",
-	    adjustMin: 0,
-	    adjustMax: 50,
-	    adjustDefault: 50
-	},
-	minorTribLoad: {
-	    name: "d",
-	    displayName: "Magnitude of Possible Persistent Bias in Lesser Tributary Sand Loads",
-	    adjustMin: 0,
-	    adjustMax: 50,
-	    adjustDefault: 50
+	var slider_defaults = {
+		bedLoad: {
+			name: "a",
+			displayName: "Bedload Coefficient for River Sand Loads",
+			adjustMin: 0,
+			adjustMax: 10,
+			adjustDefault: 5
+		},
+		riverFinesLoad: {
+			name: "e",
+			displayName: "Magnitude of Possible Persistent Bias in Measured River Silt and Clay Loads",
+			adjustMin: 0,
+			adjustMax: 25,
+			adjustDefault: 5
+		},
+		riverLoad: {
+			name: "b",
+			displayName: "Magnitude of Possible Persistent Bias in Measured River Sand Loads",
+			adjustMin: 0,
+			adjustMax: 25,
+			adjustDefault: 5
+		},
+		majorTribFinesLoad: {
+			name: "f",
+			displayName: "Magnitude of Possible Persistent Bias in Measured Major Tributary Silt and Clay Loads",
+			adjustMin: 0,
+			adjustMax: 25,
+			adjustDefault: 10
+		},
+		majorTribLoad: {
+			name: "c",
+			displayName: "Magnitude of Possible Persistent Bias in Measured Major Tributary Sand Loads",
+			adjustMin: 0,
+			adjustMax: 25,
+			adjustDefault: 10
+		},
+		minorTribFinesLoad: {
+			name: "g",
+			displayName: "Magnitude of Possible Persistent Bias in Lesser Tributary Silt and Clay Loads",
+			adjustMin: 0,
+			adjustMax: 50,
+			adjustDefault: 50
+		},
+		minorTribLoad: {
+			name: "d",
+			displayName: "Magnitude of Possible Persistent Bias in Lesser Tributary Sand Loads",
+			adjustMin: 0,
+			adjustMax: 50,
+			adjustDefault: 50
+		}
+	};
+
+	var slider = slider_defaults;
+
+	// Network specific overrides
+
+	if ("BIBE" === CONFIG.networkName) {
+		var RIVER_NAME = "Rio Grande";
+		var MAJOR_TRIB_NAME = "Tornillo Creek";
+		var LESSOR_TRIB_NAME = "Other Tributary";
+
+		slider.bedLoad.displayName = slider.bedLoad.displayName.replace("River", RIVER_NAME);
+
+		slider.riverFinesLoad.displayName = slider.riverFinesLoad.displayName.replace("River", RIVER_NAME);
+		slider.riverFinesLoad.adjustDefault = 5;
+
+		slider.riverLoad.displayName = slider.riverLoad.displayName.replace("River", RIVER_NAME);
+		slider.riverLoad.adjustDefault = 5;
+
+		slider.majorTribFinesLoad.displayName = slider.majorTribFinesLoad.displayName.replace("Major Tributary", MAJOR_TRIB_NAME);
+		slider.majorTribFinesLoad.adjustDefault = 10;
+
+		slider.majorTribLoad.displayName = slider.majorTribLoad.displayName.replace("Major Tributary", MAJOR_TRIB_NAME);
+		slider.majorTribLoad.adjustDefault = 10;
+
+		slider.minorTribFinesLoad.displayName = slider.minorTribFinesLoad.displayName.replace("Lesser Tributary", LESSOR_TRIB_NAME);
+		slider.minorTribFinesLoad.adjustMax = 50;
+
+		slider.minorTribLoad.displayName = slider.minorTribLoad.displayName.replace("Lesser Tributary", LESSOR_TRIB_NAME);
+		slider.minorTribLoad.adjustMax = 50;
 	}
-    };
+	else if ("DINO" === CONFIG.networkName) {
+		slider.bedLoad.adjustDefault = 10;
+		slider.bedLoad.adjustMax = 100;
 
-    var slider = slider_defaults;
+		slider.riverFinesLoad.adjustMin = 10;
+		slider.riverFinesLoad.adjustMax = 50;
+		slider.riverFinesLoad.adjustDefault = 10;
 
-    // Network specific overrides
+		slider.riverLoad.adjustMin = 10;
+		slider.riverLoad.adjustMax = 50;
+		slider.riverLoad.adjustDefault = 10;
 
-    if ("BIBE" === CONFIG.networkName) {
-	var RIVER_NAME = "Rio Grande";
-	var MAJOR_TRIB_NAME = "Tornillo Creek";
-	var LESSOR_TRIB_NAME = "Other Tributary";
+	}
 
-	slider.bedLoad.displayName = slider.bedLoad.displayName.replace("River", RIVER_NAME);
-
-	slider.riverFinesLoad.displayName = slider.riverFinesLoad.displayName.replace("River", RIVER_NAME);
-	slider.riverFinesLoad.adjustDefault = 5;
-
-	slider.riverLoad.displayName = slider.riverLoad.displayName.replace("River", RIVER_NAME);
-	slider.riverLoad.adjustDefault = 5;
-
-	slider.majorTribFinesLoad.displayName = slider.majorTribFinesLoad.displayName.replace("Major Tributary", MAJOR_TRIB_NAME);
-	slider.majorTribFinesLoad.adjustDefault = 10;
-
-	slider.majorTribLoad.displayName = slider.majorTribLoad.displayName.replace("Major Tributary", MAJOR_TRIB_NAME);
-	slider.majorTribLoad.adjustDefault = 10;
-
-	slider.minorTribFinesLoad.displayName = slider.minorTribFinesLoad.displayName.replace("Lesser Tributary", LESSOR_TRIB_NAME);
-	slider.minorTribFinesLoad.adjustMax = 50;
-
-	slider.minorTribLoad.displayName = slider.minorTribLoad.displayName.replace("Lesser Tributary", LESSOR_TRIB_NAME);
-	slider.minorTribLoad.adjustMax = 50;
-    }
-    else if ("DINO" === CONFIG.networkName) {
-	slider.bedLoad.adjustDefault = 10;
-	slider.bedLoad.adjustMax = 100;
-
-	slider.riverFinesLoad.adjustMin = 10;
-	slider.riverFinesLoad.adjustMax = 50;
-	slider.riverFinesLoad.adjustDefault = 10;
-
-	slider.riverLoad.adjustMin = 10;
-	slider.riverLoad.adjustMax = 50;
-	slider.riverLoad.adjustDefault = 10;
-
-    }
-
-    return {
-	sliderConfig: slider
-    };
+	return {
+		sliderConfig: slider
+	};
 }();
 

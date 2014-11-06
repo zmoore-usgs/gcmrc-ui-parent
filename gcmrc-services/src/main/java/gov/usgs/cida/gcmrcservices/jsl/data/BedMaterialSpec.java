97c7fc13ad807c47cf23666664788bdcd734b19b
David M Sibley <dmsibley@usgs.gov>
2014-11-06 14:59:38 -0600

    adding in sampleMass

--- gcmrc-services/src/main/java/gov/usgs/cida/gcmrcservices/jsl/data/BedMaterialSpec.java
+++ gcmrc-services/src/main/java/gov/usgs/cida/gcmrcservices/jsl/data/BedMaterialSpec.java
@@ -29,7 +29,8 @@ public class BedMaterialSpec extends DataSpec {
 			result = new ColumnMapping[] {
 				new ColumnMapping(ParameterSpec.C_TSM_DT, ParameterSpec.S_TSM_DT),
 				new ColumnMapping(C_SITE_NAME, S_SITE_NAME),
-				new ColumnMapping(ColumnMetadata.createColumnName(this.stationName, this.parameterCode), S_BED_VALUE, ASCENDING_ORDER, S_BED_VALUE, null, null, null, null, null, null),
+				new ColumnMapping(ColumnMetadata.createColumnName(this.stationName, this.parameterCode) + C_BED_VALUE, S_BED_VALUE),
+				new ColumnMapping(ColumnMetadata.createColumnName(this.stationName, this.parameterCode) + C_SAMPLE_MASS, S_SAMPLE_MASS),
 				new ColumnMapping(C_GROUP_NAME, S_GROUP_NAME)
 			};
 		} else {
@@ -52,22 +53,27 @@ public class BedMaterialSpec extends DataSpec {
 	@Override
 	public String setupTableName() {
 		StringBuilder result = new StringBuilder();
-
+		
 		result.append("(");
-		result.append("  SELECT BM.SAMPLE_SET,");
-		result.append("  TO_CHAR(BM.BED_MEAS_DT, 'YYYY-MM-DD\"T\"HH24:MI:SS') AS TSM_DT,");
+		result.append("  SELECT TSM_DT,");
 		result.append("  NVL(S.NWIS_SITE_NO, S.SHORT_NAME) AS SITE_NAME,");
-		result.append("  BM.BED_MEAS_DT AS SAMP_START_DT,");
-		result.append("  BM.BED_VALUE AS " + ColumnMetadata.createColumnName(this.stationName, this.parameterCode) + ",");
-		result.append("  BM.GROUP_ID,");
-		result.append("  G.NAME AS GROUP_NAME");
-		result.append(" FROM BED_MATERIAL BM,");
-		result.append("  SITE_STAR S,");
-		result.append("  GROUP_NAME G");
-		result.append(" WHERE BM.SITE_ID         = S.SITE_ID");
-		result.append("  AND BM.GROUP_ID         = G.GROUP_ID");
-		result.append("  AND G.GROUP_ID          = 15");
-		result.append(") T_A_BED_MATERIAL");
+		result.append("  VAL AS ").append(ColumnMetadata.createColumnName(this.stationName, this.parameterCode)).append(C_BED_VALUE).append(",");
+		result.append("  SAMPLE_MASS AS ").append(ColumnMetadata.createColumnName(this.stationName, this.parameterCode)).append(C_SAMPLE_MASS).append(",");
+		result.append("  SAMPLE_SET");
+		result.append("  FROM ");
+		result.append("    (SELECT");
+		result.append("    TO_CHAR(BM.BED_MEAS_DT, 'YYYY-MM-DD\"T\"HH24:MI:SS') AS TSM_DT,");
+		result.append("    SITE_ID,");
+		result.append("    max(case when BM.GROUP_ID = 15 then BM.BED_VALUE end) VAL,");
+		result.append("    max(case when BM.GROUP_ID = 14 then BM.BED_VALUE end) SAMPLE_MASS,");
+		result.append("    BM.SAMPLE_SET");
+		result.append("    FROM BED_MATERIAL BM");
+		result.append("    WHERE BM.GROUP_ID in (14, 15)");
+		result.append("    group by TO_CHAR(BM.BED_MEAS_DT, 'YYYY-MM-DD\"T\"HH24:MI:SS'),");
+		result.append("    SITE_ID, BM.SAMPLE_SET) T_A_BED,");
+		result.append("    SITE_STAR S");
+		result.append("  WHERE T_A_BED.SITE_ID = S.SITE_ID");
+		result.append(") T_A_BED_SEDIMENT");
 
 		return result.toString();
 	}
@@ -100,7 +106,9 @@ public class BedMaterialSpec extends DataSpec {
 	public static final String S_SITE_NAME = "site";
 	public static final String C_SITE_NAME = "SITE_NAME";
 	public static final String S_BED_VALUE = "bedValue";
-	public static final String C_BED_VALUE = "BED_VALUE";
+	public static final String C_BED_VALUE = "";
+	public static final String S_SAMPLE_MASS = "sampleMass";
+	public static final String C_SAMPLE_MASS = "_MASS";
 	public static final String S_GROUP_NAME = "groupName";
 	public static final String C_GROUP_NAME = "GROUP_NAME";
 	

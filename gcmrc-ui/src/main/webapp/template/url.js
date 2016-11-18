3f1c52b109839c6034712c6a0f2bc8ec5dacb2d3
Megan Hines <mhines@usgs.gov>
2016-11-18 12:08:59 -0600

    updating http to https for headers/footers/templates now that gcmrc.gov has updated their server to use https

--- gcmrc-ui/src/main/webapp/template/url.js
+++ gcmrc-ui/src/main/webapp/template/url.js
@@ -2,7 +2,7 @@
 /*This javascript take the Url of the page and writes it to the document every time the page is loaded. The only tricky part
 is that if the url is too long, it will split it into two pieces so it doesn't mess up the visual layout of the web site.
 example= there are no places for this line to break:
-http://www.gcmrc.gov/who_we_are/information_office_program/system_recovery_and_restore/system_recovery_and_restore.aspx
+https://www.gcmrc.gov/who_we_are/information_office_program/system_recovery_and_restore/system_recovery_and_restore.aspx
 and say our visual layout's width is this:
 |-------------------------------------------------------------------------------------------------------|
 the url will push the layout to fit the url, the page looks broken. So this javascript splits it into 2 lines if the url
